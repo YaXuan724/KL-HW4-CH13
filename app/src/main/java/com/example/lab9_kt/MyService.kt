c7dc22a834f7feea6bf21ba1e08cc60b8c21ebf2
YaXuan724 <t108360120@ntut.org.tw>
2021-12-16 22:06:38 +0800

    v1.0

--- app/src/main/java/com/example/lab9_kt/MyService.kt
+++ app/src/main/java/com/example/lab9_kt/MyService.kt
@@ -13,39 +13,35 @@ class MyService : Service() {
         intent?.extras?.let {
             channel = it.getString("channel", "")
         }
-        GlobalScope.launch (Dispatchers.Main){
+
+        broadcast(
+            when (channel) {
+                "music" -> "歡迎來到音樂頻道"
+                "new" -> "歡迎來到新聞頻道"
+                "sport" -> "歡迎來到體育頻道"
+                else -> "頻道錯誤"
+            }
+        )
+
+        if (::thread.isInitialized && thread.isAlive)
+            thread.interrupt()
+
+        GlobalScope.launch(Dispatchers.Main) {
+            kotlinx.coroutines.delay(3000)
             broadcast(
-                when(channel){
-                    "music" -> "歡迎來到音樂頻道"
-                    "new" -> "歡迎來到新聞頻道"
-                    "sport" -> "歡迎來到體育頻道"
+                when (channel) {
+                    "music" -> "即將播放本月TOP10音樂"
+                    "new" -> "即將為您提供獨家新聞"
+                    "sport" -> "即將播報本周NBA賽事"
                     else -> "頻道錯誤"
                 }
             )
-            if (::thread.isInitialized && thread.isAlive)
-                thread.interrupt()
-            coroutinesDelay(timeMillis = 3000)
-            thread = Thread{
-                try {
-                    broadcast(
-                        when(channel){
-                            "music" -> "即將播放本月TOP10音樂"
-                            "new" -> "即將為您提供獨家新聞"
-                            "sport" -> "即將為您播報本周NBA賽事"
-                            else -> "頻道錯誤"
-                        }
-                    )
-                }catch (e: InterruptedException){
-                    e.printStackTrace()
-                }
-            }
-            thread.start()
         }
         return START_STICKY
     }
-    override fun onBind(intent: Intent): IBinder?= null
-
-    private fun broadcast(msg: String) = sendBroadcast(Intent(channel).putExtra("msg",msg))
 
 
+    override fun onBind(intent: Intent): IBinder ?= null
+    private fun broadcast(msg: String) =
+        sendBroadcast(Intent(channel).putExtra("msg",msg))
 }
